ec65c5cba1b0f1193612546abb7f7e28635706c3
Paulo Cesar Frugeri Junior <pfrugeri@hotmail.com>
2025-06-08 01:02:54 -0300

    Adicionados componentes de login

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace P22
 {
-    partial class Form1
+    partial class frmLogin
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            txtUsuario = new TextBox();
+            label1 = new Label();
+            btLogin = new Button();
+            label2 = new Label();
+            txtSenha = new TextBox();
+            SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(50, 82);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(147, 23);
+            txtUsuario.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(50, 64);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Usuário";
+            // 
+            // btLogin
+            // 
+            btLogin.Location = new Point(50, 192);
+            btLogin.Name = "btLogin";
+            btLogin.Size = new Size(75, 23);
+            btLogin.TabIndex = 2;
+            btLogin.Text = "button1";
+            btLogin.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(50, 127);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Senha";
+            // 
+            // txtSenha
+            // 
+            txtSenha.Location = new Point(50, 145);
+            txtSenha.Name = "txtSenha";
+            txtSenha.Size = new Size(147, 23);
+            txtSenha.TabIndex = 3;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(270, 331);
+            Controls.Add(label2);
+            Controls.Add(txtSenha);
+            Controls.Add(btLogin);
+            Controls.Add(label1);
+            Controls.Add(txtUsuario);
+            Name = "frmLogin";
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtUsuario;
+        private Label label1;
+        private Button btLogin;
+        private Label label2;
+        private TextBox txtSenha;
     }
 }
